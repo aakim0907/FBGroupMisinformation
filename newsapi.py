f9c39af00f13580d0c44cb7feb1c1ceaf90d8e9a
Young Eun Kim <aakim0907@gmail.com>
2018-03-13 15:02:00 -0400

    change sources

--- newsapi.py
+++ newsapi.py
@@ -4,37 +4,33 @@ from datetime import date, timedelta
 # https://docs.python.org/3/library/datetime.html#date-objects
 
 # HANDLE INPUT 
-keyword = input("Enter keyword:")
-print (keyword)
+# keyword = input("Enter keyword:")
+# print (keyword)
 
 
 # ORIGINAL
-# url = ('https://newsapi.org/v2/everything?' 
-#        'q=bitcoin,mining&'
-#        'from=2018-01-10&'
-#        'domains=wsj.com,bbc.co.uk,economist.com,news.google.com,usatoday.com&'
-#        'sortBy=popularity&'
-#        'apiKey=ec0b1ca2e1894f9ca009e33dfc7ef619')
+url = ('https://newsapi.org/v2/everything?' 
+       'q=bitcoin&'
+       'from=2018-01-10&'
+       'sources=the-wall-street-journal,bbc-news,the-economist,google-news,usa-today&'
+       'sortBy=relevancy&'
+       'apiKey=ec0b1ca2e1894f9ca009e33dfc7ef619')
 
-# response = requests.get(url).json()
+response = requests.get(url).json()
 
 # W/ USER INPUT
-baseurl = ('https://newsapi.org/v2/everything?')
-query = 'q={}&'.format(keyword)
-date = 'from=2018-01-10&'
+# baseurl = ('https://newsapi.org/v2/everything?')
+# query = 'q={}&'.format(keyword)
+# date = 'from=2018-01-10&'
 # print (date.today())
-moreurl = ('domains=wsj.com,bbc.co.uk,economist.com,news.google.com,usatoday.com&'
-          'sortBy=popularity&'
-          'apiKey=ec0b1ca2e1894f9ca009e33dfc7ef619')
+# moreurl = ('sources=the-wall-street-journal,bbc-news,the-economist,google-news,usa-today&'
+#           'sortBy=relevancy&'
+#           'apiKey=ec0b1ca2e1894f9ca009e33dfc7ef619')
 
-response = requests.get(baseurl+query+date+moreurl).json()
+# response = requests.get(baseurl+query+date+moreurl).json()
 
 def pretty(obj):
   return json.dumps(obj, sort_keys = True, indent = 2)
 
 # A JSON object with three keys: articles, status, totalResults
 print (pretty(response))
-
-# articles = response['articles']
-# for a in articles:
-#   print (a['description'])
