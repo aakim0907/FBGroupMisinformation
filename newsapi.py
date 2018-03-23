9fe74b29c0e3a3e80d16ee179d709d32ee451aa3
Young Eun Kim <aakim0907@gmail.com>
2018-03-23 16:02:29 -0400

    create function for newsapi

--- newsapi.py
+++ newsapi.py
@@ -3,34 +3,33 @@ import json
 from datetime import date, timedelta
 # https://docs.python.org/3/library/datetime.html#date-objects
 
-# HANDLE INPUT 
-# keyword = input("Enter keyword:")
-# print (keyword)
+def get_articles(keyword_obj):
+  keyword_obj = dict((k, v) for k, v in keyword_obj.items() if v >= 4)
+  keywords = ",".join(keyword_obj.keys())
 
+  week_ago = date.today() - timedelta(days=7)
+
+  baseurl = ('https://newsapi.org/v2/everything?')
+  query = 'q={}&'.format(keywords)
+  fromdate = 'from={}&'.format(week_ago)
+  moreurl = ('sources=the-wall-street-journal,bbc-news,the-economist,google-news,usa-today&'
+            'sortBy=relevancy&'
+            'apiKey=ec0b1ca2e1894f9ca009e33dfc7ef619')
+  response = requests.get(baseurl+query+fromdate+moreurl).json()
+
+  return [article['url'] for article in response['articles']]
 
 # ORIGINAL
-url = ('https://newsapi.org/v2/everything?' 
-       'q=bitcoin&'
-       'from=2018-01-10&'
-       'sources=the-wall-street-journal,bbc-news,the-economist,google-news,usa-today&'
-       'sortBy=relevancy&'
-       'apiKey=ec0b1ca2e1894f9ca009e33dfc7ef619')
+# url = ('https://newsapi.org/v2/everything?' 
+#        'q=bitcoin&'
+#        'from=2018-01-10&'
+#        'sources=the-wall-street-journal,bbc-news,the-economist,google-news,usa-today&'
+#        'sortBy=relevancy&'
+#        'apiKey=ec0b1ca2e1894f9ca009e33dfc7ef619')
+# response = requests.get(url).json()
 
-response = requests.get(url).json()
-
-# W/ USER INPUT
-# baseurl = ('https://newsapi.org/v2/everything?')
-# query = 'q={}&'.format(keyword)
-# date = 'from=2018-01-10&'
-# print (date.today())
-# moreurl = ('sources=the-wall-street-journal,bbc-news,the-economist,google-news,usa-today&'
-#           'sortBy=relevancy&'
-#           'apiKey=ec0b1ca2e1894f9ca009e33dfc7ef619')
-
-# response = requests.get(baseurl+query+date+moreurl).json()
-
-def pretty(obj):
-  return json.dumps(obj, sort_keys = True, indent = 2)
+# def pretty(obj):
+#   return json.dumps(obj, sort_keys = True, indent = 2)
 
 # A JSON object with three keys: articles, status, totalResults
-print (pretty(response))
+# print (pretty(response))
