cf0e2194f1db4655c0dc48b48af9a337af7077d9
unknown <chowcy@umich.edu>
2018-03-27 15:49:33 -0400

    make list of p tags into string

--- scrapper.py
+++ scrapper.py
@@ -7,7 +7,7 @@ indiatv = "https://www.indiatvnews.com/entertainment/celebrities-okay-for-outsid
 bbc = "http://www.bbc.com/news/world-us-canada-43444791"
 usatoday = "https://www.usatoday.com/story/tech/talkingtech/2018/01/31/facebook-says-no-cryptocurrency-ads-citing-misleading-and-deceptive-practices/1082117001/?utm_source=google&utm_medium=amp&utm_campaign=speakabl"
 economist = "https://www.economist.com/news/finance-and-economics/21735055-china-has-taken-harsh-line-south-korea-contemplates-banning-bitcoin"
-URL = economist
+
 
 # extracter code
 def scrape_from_url(URL):
@@ -19,4 +19,5 @@ def scrape_from_url(URL):
         for x in tag.descendants:
             if len(x) > 0 and str(type(x)) == "<class 'bs4.element.NavigableString'>":
                 extracted.append(x)
-    return extracted
+    #make into one string            
+    return ' '.join(extracted)
