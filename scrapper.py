cb610d28b62acf9b2c6be04d8434e1f8c7deb185
unknown <chowcy@umich.edu>
2018-04-06 14:03:38 -0400

    comment out debugging print statements

--- scrapper.py
+++ scrapper.py
@@ -6,14 +6,14 @@ indiatv = "https://www.indiatvnews.com/entertainment/celebrities-okay-for-outsid
 bbc = "http://www.bbc.com/news/world-us-canada-43444791"
 usatoday = "https://www.usatoday.com/story/tech/talkingtech/2018/01/31/facebook-says-no-cryptocurrency-ads-citing-misleading-and-deceptive-practices/1082117001/?utm_source=google&utm_medium=amp&utm_campaign=speakabl"
 economist = "https://www.economist.com/news/finance-and-economics/21735055-china-has-taken-harsh-line-south-korea-contemplates-banning-bitcoin"
-
+teaparty = "https://www.teaparty.org/iraq-war-vet-lost-leg-battle-shuts-david-hogg-brutal-message-298358/"
 
 # extracter code
 def scrape_from_url(URL):
     r = requests.get(URL)
     soup = BeautifulSoup(r.content,"html.parser")
     title = soup.title.string
-    print('title is ', title)
+    # print('title is ', title)
     #get all text in h1s
     h1tags = soup.find_all('h1')
     extracted = []
@@ -22,11 +22,11 @@ def scrape_from_url(URL):
                 if len(x) > 0 and str(type(x)) == "<class 'bs4.element.NavigableString'>":
                     extracted.append(x)
     h1_string = " ".join(extracted)
-    print('h1s are ', h1_string)
+    # print('h1s are ', h1_string)
     #cross check title with h1 and only include those that are in both
     title_words = title.split()
     h1_words = h1_string.split()
     headline = " ".join([word for word in title_words if word in h1_words])
     return headline
 
-# print(scrape_from_url(economist))
+# print(scrape_from_url(indiatv))
