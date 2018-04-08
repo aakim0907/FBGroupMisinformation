2f6832c181826a7465917eb1277a01487f98f071
Huyen Phan <huyenttphan@gmail.com>
2018-04-08 18:02:20 -0400

    removed paralleldots, cleaned up codes

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ import sys
 import retinasdk
 import config
 import webbrowser
+from bs4 import BeautifulSoup
 
 #from our code
 from scraper import scrape_from_url
@@ -21,6 +22,10 @@ def index(results=None):
     if request.args.get('link', None):
         article = request.args.get('link', None)
         results = get_similar_urls(article)[:5]
+        # for result in results:
+        #     r = requests.get(result)
+        #     soup = BeautifulSoup(r.content,"html.parser")
+        #     title = soup.title.string
     return render_template('index.html', results=results)
 
 
@@ -41,3 +46,6 @@ def get_similar_urls(url):
 #     print(url)
 #     opens it in browser so you can see the news articles right away and see if relevant
 #     webbrowser.get().open(url)
+
+if __name__ == "__hello__":
+    app.run()
--- paralleldots.py
+++
@@ -1,8 +0,0 @@
-import paralleldots
-
-api_key   = "4FJEnSvGxUI6FCYl64yWK2iDE4AqZpQso150wqrilr0"
-text      = "Prime Minister Narendra Modi tweeted a link to the speech Human Resource Development Minister Smriti Irani made in the Lok Sabha during the debate on the ongoing JNU row and the suicide of Dalit scholar Rohith Vemula at the Hyderabad Central University."
-
-paralleldots.set_api_key(api_key)
-
-print( paralleldots.keywords( text ) )
